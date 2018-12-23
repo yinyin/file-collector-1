276b971997ddd8e80501ce3293973cc809943500
Yinyin L <yinyinl@gmail.com>
2018-12-24 01:27:30 +0800

    define interface of collecting with reader

--- state.go
+++ state.go
@@ -1,6 +1,9 @@
 package filecollector1
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"io"
+)
 
 // FileState represent state of one result file
 type FileState struct {
@@ -13,4 +16,16 @@ type FileState struct {
 // CollectState represents state of collecting
 type CollectState struct {
 	FileStates map[string]*FileState
+}
+
+// CollectWithReader collects content from given `reader` and save into file
+// specified by `destination`.
+func (state *CollectState) CollectWithReader(destination *CollectDest, reader io.Reader, sourceFilePath string) (err error) {
+	existedState, ok := state.FileStates[destination.ToPath]
+	if ok {
+		// TODO: check if check-sum is the same one and update state
+	} else {
+		// TODO: duplicate to target path and add state
+	}
+	return nil
 }
